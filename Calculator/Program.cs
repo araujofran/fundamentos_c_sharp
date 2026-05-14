9af9cbf6c8cb597475ccb99816217b8b2047fe5f
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2026-05-14 17:49:28 -0300

    configura gitignore e organiza projetos .NET

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,20 +7,96 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Menu();
+        }
+        static void Soma()
+        {
             Console.Clear();
-            Console.Write("Digite o Primeiro Numero:");
+            Console.WriteLine("Digite o Primeiro Numero:");
             float v1 = float.Parse(Console.ReadLine());
 
-            Console.Write("Digite um segundo valor:");
+            Console.WriteLine("Digite um segundo valor:");
             float v2 = float.Parse(Console.ReadLine());
-
 
             Console.WriteLine("O número digitado foi:" + v1 + ". O segundo numero foi de:" + v2 + ".");
 
             float r_soma = v1 + v2;
             Console.WriteLine("A soma é de :" + r_soma);
 
-            
+            Console.ReadKey();
+            Console.WriteLine();
+            Menu();
+        }
+        static void Subtracao()
+
+        {
+            Console.Clear();
+            Console.WriteLine("Digite um numero:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro numero:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado dessa subtração é de {v1 - v2}");
+            Console.ReadKey();
+            Console.WriteLine();
+            Menu();
+
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite um numero:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro nome:");
+            float v2 = float.Parse(Console.ReadLine());
+            Console.WriteLine($"O resultado dessa divisão foi de {v1 / v2}");
+            Console.ReadKey();
+            Console.WriteLine();
+            Menu();
+
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite um numero:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um segundo numero:");
+            float v2 = float.Parse(Console.ReadLine());
+            Console.WriteLine($"A multiplicação entre os dois números é de {v1 * v2}");
+            Console.ReadKey();
+            Console.WriteLine();
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("============================================================");
+            Console.WriteLine("=============CALCULADORA INTELIGENTE C#=====================");
+            Console.WriteLine("Escolha uma das opções:");
+            Console.WriteLine("[1] SOMA");
+            Console.WriteLine("[2] SUBTRAÇÃO");
+            Console.WriteLine("[3] DIVISÃO");
+            Console.WriteLine("[4] MULTIPLICAÇÃO");
+            Console.WriteLine("[5] SAIR");
+            Console.WriteLine("============================================================");
+            Console.WriteLine();
+            Console.WriteLine("SELECIONE!!!");
+            short resp = short.Parse(Console.ReadLine());
+            switch(resp)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("FIM!!!");
 
         }
     }
