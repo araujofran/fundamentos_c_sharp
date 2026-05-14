1d52320fbbb44e039a94868d63c81f2cd1001680
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2026-05-14 18:10:52 -0300

    organiza estrutura e remove artefatos .NET

--- Aulas_balta/Program.cs
+++ Aulas_balta/Program.cs
@@ -4,15 +4,14 @@ namespace Aulas_balta;
 
 class Program
 {
+    //Fundamentos de C# - Variáveis e Tipos de Dados
     static void Main(string[] args)
     {
-        string texto = "Testando o VS Code";
-        Console.WriteLine(texto); 
-        
-        // Para testar o autocompletar:
-        // Digite 'texto.' na linha abaixo e aguarde 1 segundo.
+        int idade = 30;
+        string nome = "Balta";
+        Console.WriteLine(idade + " anos." + " O nome é " + nome + "!!!");
+        string texto = "Fundamentos de C#!!!";
+        Console.WriteLine(texto);
 
-        
-        
     }
 }
---
+++ Cronometro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cronometro  
+{
+    class Program {
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
