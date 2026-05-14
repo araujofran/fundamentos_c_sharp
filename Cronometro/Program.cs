20e7d9abcb223a12fc026cc7c49f6c4fad7e2882
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2026-05-14 18:44:25 -0300

    adiciona cronometro com spinner animado

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading;
 
-namespace Cronometro  
+namespace Cronometro
 {
-    class Program {
+    class Program
+    {
 
         static void Main(string[] args)
         {
-            
+            Start();
+        }
+        static void Start()
+        {
+            int time = 61;
+            int index = 0;
+            int currentTime = 0;
+            string[] spinner = { "/", "-", "\\", "|" };
+
+            while (currentTime != time)
+            {
+                Console.Write($"\r{spinner[index]} Tempo: {currentTime}s");
+
+                Thread.Sleep(1000);
+
+                currentTime++;
+
+                index++;
+
+                if (index == spinner.Length)
+                    index = 0;
+
+
+            }
+            Console.WriteLine("\nUm minuto! Acabou o Tempo!");
         }
     }
 }
