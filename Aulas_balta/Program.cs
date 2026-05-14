bb05238fe5a7ed469457b6eee233c185fe01cf29
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2026-05-14 13:32:51 -0300

    adiciona aulas balta

---
+++ Aulas_balta/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aulas_balta;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string texto = "Testando o VS Code";
+        Console.WriteLine(texto); 
+        
+        // Para testar o autocompletar:
+        // Digite 'texto.' na linha abaixo e aguarde 1 segundo.
+
+        
+        
+    }
+}
